a4a98a900b98f4101db04116eac5e86e73d8638b
Patrick Holton <holtonpk@gmail.com>
2022-06-10 13:57:15 -0600

    img import

--- src/components/LoadingFallback.jsx
+++ src/components/LoadingFallback.jsx
@@ -1,10 +1,8 @@
-import logo from '../../public/goodslogo2.svg';
-
 export default function LoadingFallback() {
   return (
     <div className="fixed z-40 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-      <div className="origin-center  w-36 h-36 spin">
-        <img src={logo} alt="" className="w-full h-full" />
+      <div className="origin-center w-36 h-36 spin">
+        <img src="goodslogo2.svg" alt="" className="w-full h-full" />
       </div>
     </div>
   );
